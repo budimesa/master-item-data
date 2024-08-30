18e6cae2f587c007e316ba399805a5a851c5738f
budimesa <budihmesa@gmail.com>
2024-08-30 14:20:50 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        "./resources/**/*.{js,vue,ts,css}",
+        // './resources/**/*.{vue, js, jsx, ts, tsx}',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [
+        forms, // Jika Anda menggunakan plugin Tailwind CSS Forms
+        require('tailwindcss-primeui') // Tambahkan plugin Tailwind CSS PrimeUI
+    ],
+};
